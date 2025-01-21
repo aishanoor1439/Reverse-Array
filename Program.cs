5a2f3aac22e09e5840bf504b831cc6aa73211ca8
aishanoor1439 <aisha.noor1439@gmail.com>
2025-01-21 09:44:30 +0100

    Final Commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+
+            Console.WriteLine("Enter 10 integers:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Enter number {i + 1}: ");
+                numbers[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nDuplicate numbers and their indexes:");
+
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = i + 1; j < 10; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        Console.WriteLine($"Duplicate number: {numbers[i]}, found at index {i} and {j}");
+                    }
+                }
+            }
+
+        }
+    }
+}
